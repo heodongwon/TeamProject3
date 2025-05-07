3df5270e7052b2231a2003cf19517db1be2a0ad2
heodongwon <ehddnjs6409@naver.com>
2025-05-07 11:28:13 +0900

    refact

--- src/main/java/com/example/teamproject3/controller/IndexController.java
+++ src/main/java/com/example/teamproject3/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.teamproject3;
+package com.example.teamproject3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
