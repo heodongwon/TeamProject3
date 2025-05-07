eab753af6ea6fb7228c2ee301302480cdedce36c
heodongwon <ehddnjs6409@naver.com>
2025-05-07 11:21:10 +0900

    init

---
+++ src/main/java/com/example/teamproject3/Teamproject3Application.java
@@ -0,0 +1,13 @@
+package com.example.teamproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teamproject3Application.class, args);
+	}
+
+}
